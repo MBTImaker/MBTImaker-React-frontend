8824f71f37a29f5f3980f58f51b125694dade96c
hotbreakb <areyhappy12@gmail.com>
2022-06-05 17:38:36 +0900

    Remove: never used

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react'
 
 const useImage = (id: number) => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<any>();
     const [image, setImage] = useState(null);
 
     useEffect(() => {
@@ -10,21 +8,15 @@ const useImage = (id: number) => {
             try {
                 const response = await import(`../assets/images/text/id/${id}.png`);
                 setImage(response.default);
-            } catch (err) {
-                setError(err);
-            } finally {
-                setLoading(false);
+            } catch (e) {
+                console.error(e);
             }
         }
 
         fetchImage();
     }, [id])
 
-    return {
-        loading,
-        error,
-        image,
-    }
+    return { image }
 }
 
 export default useImage;
